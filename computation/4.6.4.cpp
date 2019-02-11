5fcdb87df9dedd2f7837a0b43644d738ccaebc91
Michael Mikulak <mpmikulak@gmail.com>
2019-02-11 16:24:15 -0600

    Update 4.6.4.cpp

--- computation/4.6.4.cpp
+++ computation/4.6.4.cpp
@@ -12,18 +12,35 @@ double calculate_mean(std::vector<double> v)
   return total / v.size();
 }
 
+double calculate_median(std::vector<double> v)
+{
+  std::sort(v.begin(),v.end());
+  
+  double median = 0;
+  int middle = v.size() / 2;
+  
+  if (v.size() % 2 == 0)
+  {
+    median = (v[middle] + v[--middle])/2;
+  }
+  else
+  {
+    return v[middle];  
+  }
+  return median;
+}
+
 int main()
 {
-  std::vector<double> temps;
+  std::vector<double> temps = {23.57, 24.65, 74.165, 682.65, 73.66, 509.36, 324.76, 234.56};
 
-  for (double temp; std::cin >> temp;)
-  {
-    temps.push_back(temp);
-  } 
-
-  std::cout << "Average tempurature: " << calculate_mean(temps) << '\n';
+  // for (double temp; std::cin >> temp;)
+  // {
+  //   temps.push_back(temp);
+  // } 
   std::sort(temps.begin(), temps.end());
-  std::cout << "Mean temp: " << temps[temps.size()/2] << '\n';
-
-  std::cout << "\nend\n";
+  for (auto t : temps) std::cout << t << " ";
+  std::cout << '\n';
+  std::cout << "Average temperature: " << calculate_mean(temps) << '\n';
+  std::cout << "Median temperature: " << calculate_median(temps) << '\n';
 }
